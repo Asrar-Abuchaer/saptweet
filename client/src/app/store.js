80b252fb5aedfd50641cc5aaad96ef4fc7fb8bde
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-10-30 20:14:10 +0700

    little update

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,12 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import postReducer from "../features/posts/postSlice";
 import { AuthReducer } from "../features/users/authReducers";
-import usersReducer from "../features/userReducer/userReducer";
 
 export default configureStore({
   reducer: {
     posts: postReducer,
     AuthReducer: AuthReducer,
-    users: usersReducer,
   },
 });
